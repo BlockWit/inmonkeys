536de09d35aa7b2ec9232b45ec996e0c1018914f
Alexander Strakh <cromlehg@gmail.com>
2022-01-30 20:26:33 +0300

    Controller for cromlehg has been added

---
+++ src/main/java/com/blockwit/inmonkeys/controller/cromlehg/CromlehgController.java
@@ -0,0 +1,19 @@
+package com.blockwit.inmonkeys.controller.cromlehg;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/cromlehg")
+public class CromlehgController {
+
+	@GetMapping
+	@ResponseBody
+	public String home() {
+		return "Hello, cromlehg";
+	}
+
+
+}
